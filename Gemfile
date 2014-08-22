703331c648f475be66c5877f4621b8072086c637
Andrew Neely <andrew.neely@agapered.com>
2014-08-22 17:38:33 -0500

    initial commit with methods to return Vanilla Ice as a resource, resized by a percentage, or sized to a specific set of dimensions with padding

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'json'
+gem 'curb'
+gem 'shotgun'
+gem 'mini_magick'
+gem 'pry'
+
---
+++ ice_tre.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require 'json'
+# for pretty debugging output
+require 'pp'
+require 'mini_magick'
+require 'pry'
+
+helpers do
+
+end
+
+# trying to test if mini_magick is installed correctly
+get '/vanilla_ice' do
+  image = MiniMagick::Image.open('image/vanilla_ice_white_bg.jpg')
+  content_type 'image/jpg'
+  image.to_blob
+end
+
+get '/vanilla_ice/:percentage/percent' do
+  percentage = "#{params[:percentage]}".to_i
+  if percentage > 0
+    image = MiniMagick::Image.open('image/vanilla_ice_white_bg.jpg')
+    image.sample("#{percentage}%")
+    content_type 'image/jpg'
+    image.to_blob
+  else
+    redirect('/vanilla_ice')
+  end
+end
+
+get '/vanilla_ice/:width/width/:height/height' do
+  width = "#{params[:width]}".to_i
+  height = "#{params[:height]}".to_i
+  color = '#FFFFFF'
+  if width > 0 && height > 0
+    image = MiniMagick::Image.open('image/vanilla_ice_white_bg.jpg')
+    image.combine_options do |img|
+      img.thumbnail("#{width}x#{height}>")
+      img.background(color)
+      img.gravity('South')
+      img.extent("#{width}x#{height}")
+    end
+    content_type 'image/jpg'
+    image.to_blob
+  else
+    redirect('/vanilla_ice')
+  end
+end
+
+# route methods for undefined API requests
+not_found do
+  error 400
+end
+
+error 401 do
+  "{'error':'unauthorized', 'message':'You are not authorized to access this resource.'}"
+end
