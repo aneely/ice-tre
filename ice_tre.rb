b455ef2f8e0c2474313457e0c767af4fe51a236b
Andrew Neely <andrew.neely@agapered.com>
2014-08-28 18:55:57 -0500

    extracting logic into helpers and refactoring basic url scheme to allow abstract rappers

--- ice_tre.rb
+++ ice_tre.rb
@@ -5,7 +5,31 @@ require 'mini_magick'
 require 'pry'
 
 helpers do
+  def valid_width_and_height?(height, width)
+    width > 0 && height > 0
+  end
 
+  def valid_hex_color?(color)
+    match = color.match(/^#([0-9a-fA-F]{3}){1,2}$/)
+    match.to_s.length > 0
+  end
+
+  def assign_transparent_rapper_png(rapper)
+    begin
+      MiniMagick::Image.open("image/#{rapper}_transp.png")
+    rescue
+      false
+    end
+  end
+
+  def resize_and_pad_with_color(image, width, height, color)
+    image.combine_options do |img|
+      img.thumbnail("#{width}x#{height}>")
+      img.background(color)
+      img.gravity('South')
+      img.extent("#{width}x#{height}")
+    end
+  end
 end
 
 # trying to test if mini_magick is installed correctly
@@ -15,7 +39,7 @@ get '/vanilla_ice' do
   image.to_blob
 end
 
-get '/vanilla_ice/:percentage/percent' do
+get '/rapper/vanilla_ice/:percentage/percent' do
   percentage = "#{params[:percentage]}".to_i
   if percentage > 0
     image = MiniMagick::Image.open('image/vanilla_ice_white_bg.jpg')
@@ -27,11 +51,12 @@ get '/vanilla_ice/:percentage/percent' do
   end
 end
 
-get '/vanilla_ice/:width/width/:height/height' do
+get '/rapper/vanilla_ice/width/:width/height/:height' do
   width = "#{params[:width]}".to_i
   height = "#{params[:height]}".to_i
   color = '#FFFFFF'
-  if width > 0 && height > 0
+
+  if valid_width_and_height?(height, width)
     image = MiniMagick::Image.open('image/vanilla_ice_white_bg.jpg')
     image.combine_options do |img|
       img.thumbnail("#{width}x#{height}>")
@@ -39,6 +64,21 @@ get '/vanilla_ice/:width/width/:height/height' do
       img.gravity('South')
       img.extent("#{width}x#{height}")
     end
+    content_type 'image/jpg'
+    image.to_blob
+  else
+    redirect('/vanilla_ice')
+  end
+end
+
+get '/rapper/:rapper/width/:width/height/:height/color/:color' do
+  image  =  assign_transparent_rapper_png(params[:rapper])
+  width  =  "#{params[:width]}".to_i
+  height =  "#{params[:height]}".to_i
+  color  = "##{params[:color]}"
+
+  if image and valid_width_and_height?(height, width) and valid_hex_color?(color)
+    resize_and_pad_with_color(image, width, height, color)
     content_type 'image/jpg'
     image.to_blob
   else
